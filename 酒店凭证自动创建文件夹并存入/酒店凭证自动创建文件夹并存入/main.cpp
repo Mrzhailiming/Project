b5529d2488aa280738bd26c7d866339f7cb49513
Mrzhailiming <401875751@qq.com>
2020-11-14 18:17:42 +0800

    LPCWSTR就是const wchar*

--- 酒店凭证自动创建文件夹并存入/酒店凭证自动创建文件夹并存入/main.cpp
+++ 酒店凭证自动创建文件夹并存入/酒店凭证自动创建文件夹并存入/main.cpp
@@ -8,7 +8,7 @@ void bg(){
 	char path[40] = { 0 };
 	myWork w(path);
 	//得到所有文件
-	w.getAllFile();
+	w.getAllFile(path);
 	//处理
 	w.create_moveFile();
 	cout << "********chuli成功*******" << endl;
@@ -43,9 +43,15 @@ void menu(){
 }
 void testGetAllFile(){
 	myWork m("D:\\test");
-	m.getAllFile();
+	m.getAllFile("D:\\test");
 
 	m.num_fimeName();
+	wchar_t old = "D:\\test\\其他应付款 216 217 银行89 (2).zip";
+	LPCWSTR* old = new LPCWSTR();
+	LPCWSTR* newPath = new LPCWSTR("D:\\");
+	MoveFile(*old, *newPath);
+	//m.create_moveFile();
+	//m.createNewDir("D:\\aa");
 }
 
 
--- 酒店凭证自动创建文件夹并存入/酒店凭证自动创建文件夹并存入/test.h
+++ 酒店凭证自动创建文件夹并存入/酒店凭证自动创建文件夹并存入/test.h
@@ -18,31 +18,39 @@ using namespace std;
 
 class myWork{
 public:
-	myWork(string path = NULL){;
+	myWork(string pt = NULL){
+		path = pt;
 	}
 
 	//获取文件夹下所有文件
-	void getAllFile(){
-		_finddata_t findData;
-		long fHandle;
-		fHandle = _findfirst("D:\\test\\*.*", &findData);
-		if (fHandle != -1){
-			do {
-				if (findData.name == "." || findData.name == ".."){
-					continue;
-				}
-				
-				pfileVec.push_back(findData.name);
-				
-			} while (!_findnext(fHandle, &findData));
+	void getAllFile(const string& path){
+		const std::string tar = path + "\\" + "*.*";
+		_finddata_t fileData;
+		long handle = _findfirst(tar.c_str(), &fileData);
+		if (handle == -1){
+			perror("find file Error : ");
+			_findclose(handle);
+			return;
 		}
+		do{
+			if (fileData.attrib & _A_SUBDIR){
+				if (strcmp(fileData.name, ".") != 0 && strcmp(fileData.name, "..") != 0){
+					getAllFile(path + "\\" + fileData.name);
+				}
+			}
+			else{
+				pfileVec.push_back(path + "\\" + fileData.name);
+			}
+		} while (_findnext(handle, &fileData) == 0);
+		_findclose(handle);
 	}
 	//以序号-文件名映射， multimap
 	void num_fimeName(){
 		//遍历vector, 把序号和文件名映射
-		int num = 0;
+		
 		for (const auto& e : pfileVec){
 			int i = 0;
+			int num = 0;
 			while (i < e.size() && (e[i] < '0' || e[i] > '9')){
 				++i;
 			}
@@ -73,19 +81,21 @@ public:
 			moveFile(oldPath, newPath);
 			++it;
 			if (it->first == num){
-
+				oldPath = path + it->second;
+				moveFile(oldPath, newPath);
+				++it;
 			}
 		}
 	}
 
 	//创建对应公司/人的文件夹（以序号代替）
-	void createNewDir(string& dirPath_name){
+	void createNewDir(const string& dirPath_name){
 		string sh = "md ";
 		string tmp = sh + dirPath_name;
 		system(tmp.c_str());
 	}
 	//移动文件
-	void moveFile(string& oldPath, string& newPath){
+	void moveFile(const string& oldPath, const string& newPath){
 			MoveFile((LPCWSTR)oldPath.c_str(), (LPCWSTR)newPath.c_str());
 	}
 private:
